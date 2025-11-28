51f044026584c27cb21112fd0c0224384ebb15f9
kava117 <auraelya@live.com>
2025-11-28 15:40:52 -0500

    feat: added location tracking to map
    map will not show "inaccessible" buttons to the player, will only show you what is next up

--- SporeStorm/Assets/Scripts/MapPanelUI.cs
+++ SporeStorm/Assets/Scripts/MapPanelUI.cs
@@ -13,9 +13,76 @@ public class MapPanelUI : MonoBehaviour
 
     public void LoadMap()
     {
+        LoadMapButtons(worldstate.GetLocation());
+
         fuelAmountLeft.text = worldstate.GetFuel() + "";
 
         // BRIANNE, YOUR STORM CODE GOES HERE
+    }
+
+    private void LoadMapButtons(string currentLocation)
+    {
+        // turn off all of them to start
+        foreach (MapButtonBehavior mapButton in mapButtons)
+        {
+            mapButton.gameObject.SetActive(false);
+        }
+
+        if (currentLocation == "Florida")
+        {
+            //mississippi missouri
+            mapButtons[0].gameObject.SetActive(true); // missouri
+            mapButtons[1].gameObject.SetActive(true); // mississippi
+        }
+        else if (currentLocation == "Mississippi")
+        {
+            //nebraska
+            mapButtons[3].gameObject.SetActive(true); // nebraska
+        }
+        else if (currentLocation == "Missouri")
+        {
+            //nebraska illinois
+            mapButtons[2].gameObject.SetActive(true); // illinois
+            mapButtons[3].gameObject.SetActive(true); //nebraska
+        }
+        else if (currentLocation == "Nebraska")
+        {
+            //south dakota north dakota
+            mapButtons[4].gameObject.SetActive(true); // north dakota
+            mapButtons[5].gameObject.SetActive(true); // south dakota
+        }
+        else if (currentLocation == "Illinois")
+        {
+            //north dakota
+            mapButtons[4].gameObject.SetActive(true); // north dakota
+        }
+        else if (currentLocation == "SouthDakota")
+        {
+            //Wyoming
+            mapButtons[6].gameObject.SetActive(true); // wyoming
+        }
+        else if (currentLocation == "NorthDakota")
+        {
+            //wyoming
+            mapButtons[6].gameObject.SetActive(true); // wyoming
+        }
+        else if (currentLocation == "Wyoming")
+        {
+            //Idaho
+            mapButtons[7].gameObject.SetActive(true); // idaho
+        }
+        else if (currentLocation == "Idaho")
+        {
+            //Washington utah arizona
+            mapButtons[10].gameObject.SetActive(true); // washington
+            mapButtons[8].gameObject.SetActive(true); // utah
+            mapButtons[9].gameObject.SetActive(true); // arizona
+        }
+        else if (currentLocation == "Utah")
+        {
+            //arizona
+            mapButtons[9].gameObject.SetActive(true); // arizona
+        }
     }
 
     public void ChangeNextLocation(string locationName)
